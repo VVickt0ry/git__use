a236f41fe09351862d611de1a47928538635c1b1
VVickt0ry <1547865413@qq.com>
2026-02-23 10:40:59 +0800

    git的报错信息

---
+++ error__info.py
@@ -0,0 +1,50 @@
+
+# 1. To https://github.com/VVickt0ry/git__use.git
+#  ! [rejected]        main -> main (fetch first)
+# error: failed to push some refs to 'https://github.com/VVickt0ry/git__use.git'
+# hint: Updates were rejected because the remote contains work that you do not have locally.
+
+# ! [rejected]：表示推送被拒绝
+# 关键原因：remote contains work that you do not have locally（远程有你本地没有的内容）
+# Git的推送规则是：只有当本地分支包含远程分支的所有提交记录时，才能成功推送（相当于 “你的本地仓库必须是远程仓库的‘超集’”）
+
+# 拉取远程最新内容并合并到本地
+# 处理合并冲突（仅当步骤 1 提示冲突时需要）
+# 重新推送本地提交到远程
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
